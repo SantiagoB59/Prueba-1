c4c86eebdb4d8ce044fd97afed3dee0e19d18909
SantiagoB59 <75129125+SantiagoB59@users.noreply.github.com>
2022-06-07 17:42:23 -0500

    Update Ing_sw_2022_1.java

--- ing_sw_2022_1/src/ing_sw_2022_1/Ing_sw_2022_1.java
+++ ing_sw_2022_1/src/ing_sw_2022_1/Ing_sw_2022_1.java
@@ -6,6 +6,8 @@ public class Ing_sw_2022_1 {
 
     public static void main(String[] args) {
         // TODO code application logic here
+        
+        System.out.println("Hola mundo_prueba (punto 13)");
     }
     
 }
